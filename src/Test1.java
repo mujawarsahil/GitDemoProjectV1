ca1ca6b3f816fdb3bac440ddcf894fa3ccb2f014
exam <exam@Pc-10>
2022-08-01 13:03:03 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		String hungry="Yes";
+		if(hungry.equals("Yes"))
+		{
+			System.out.println("Eat Something....");
+		}
+		else
+		{
+			System.out.println("Do some work!!!");
+		}
+	}
+
+}
